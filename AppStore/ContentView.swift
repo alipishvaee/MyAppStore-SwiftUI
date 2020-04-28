995528fcbf456cf221874ef1c762d1a5469fce13
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-04-28 22:02:04 +0430

    Initial Commit

---
+++ AppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStore
+//
+//  Created by Ali Pishvaee on 4/28/20.
+//  Copyright © 2020 iPersianDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
